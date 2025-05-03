d31a180986a4e2bce46d5ff442fd8b3ff23eb335
AndersKoo <anderskoo@live.no.com>
2025-05-03 18:01:38 +0200

    cleanup

--- src/ui/Divider.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Divider = styled.div`
-  width: 100%;
-  max-width: 250px;
-  height: 2px;
-  border: none;
-  background-color: #ccc;
-`;
--- src/ui/Line.tsx
+++ src/ui/Line.tsx
@@ -1,7 +1,15 @@
-import { Divider } from "./Divider";
+import styled from "styled-components";
 
 const Line: React.FC = () => {
   return <Divider />;
 };
+
+const Divider = styled.div`
+  width: 100%;
+  max-width: 250px;
+  height: 2px;
+  border: none;
+  background-color: #ccc;
+`;
 
 export default Line;
