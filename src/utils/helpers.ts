0059e3b72f00142cdb1198c4356c384e6a581266
AndersKoo <anderskoo@live.no.com>
2025-05-03 23:58:02 +0200

    cleaner

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,10 +1,11 @@
 import { Validation } from "./type";
 
 export const priceValidation = (input: string) => {
+  const partiallyFilled = /^\d*\.?\d*$/;
   const regex = /^\d+(\.\d+)?$/;
   if (regex.test(input)) return Validation.APPROVED;
   if (input.trim() === "") return Validation.EMPTY;
-  if (/^\d*\.?\d*$/.test(input)) return Validation.PARTIALLYFILLED; // f.eks .45, 1.34
+  if (partiallyFilled.test(input)) return Validation.PARTIALLYFILLED; // f.eks .45, 1.34
   return Validation.INVALID;
 };
 
